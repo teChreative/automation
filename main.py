b35190945e48ed478c2dbe4746e7bb3d64444f9e
techreative <nathanielejones@hotmail.com>
2022-08-08 23:23:47 -0400

    added selenium webdriver and outlined test case

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('this is a python file')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get('http://www.google.com')
+
+# Go to sign in screen
+# Find and enter into username field
+# Find and enter password
+# Find, pause, and select Login button
+# find and select Settings
+# find and select logout

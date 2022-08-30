8400ecef72a43e8d2de3148966862a7a26193ac1
techreative <nathanielejones@hotmail.com>
2022-08-29 20:48:49 -0400

    Open ESPN site and selected a sport from the menu.

--- espn.py
+++ espn.py
@@ -3,32 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# 1 - go to ESPN
+# 1 - Go to ESPN
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.espn.com/')
 
-# 2 - hover over bar
-# driver.find_element(
-#     By.CSS_SELECTOR, '#global-nav > ul').send_keys(Keys.RETURN)
 time.sleep(2)
 
-# hoverDOTS = ActionChains(driver)
-# DT = driver.find_element(
-#     By.CSS_SELECTOR, '#global-nav > ul')
-# hoverDOTS.move_to_element(DT).perform()
-
-# select three dot
+# 2 - Select from menu
+# THREE DOTS - #global-nav > ul > li.none.more > a
+# TENNIS - #global-nav > ul > li.sports.menu-tennis > a
 driver.find_element(
-    By.CSS_SELECTOR, '#main-container > div > section.col-one > article:nth-child(1) > div > ul > li.quicklinks_list__item.quicklinks_list__item--eplus-paywall > a').send_keys(Keys.RETURN)
+    By.CSS_SELECTOR, '#global-nav > ul > li.none.more > a').send_keys(Keys.RETURN)
 time.sleep(2)
 
-# 3 - select sport - BOXING
+# 3 - Select sport
+# BOXING - #global-nav > ul > li.none.more.hover > div > ul > li.sports.menu-boxing > a
+# TENNIS - #global-nav > ul > li.sports.menu-tennis.hover > div > ul > li:nth-child(1) > a
 driver.find_element(
     By.CSS_SELECTOR, '#global-nav > ul > li.none.more.hover > div > ul > li.sports.menu-boxing > a').send_keys(Keys.RETURN)
-time.sleep(2)
+time.sleep(3)
 
-
-# driver.find_element(
-#     By.CSS_SELECTOR, '').send_keys('')
-# time.sleep(2)
+# 4 - close browser
+driver.close()

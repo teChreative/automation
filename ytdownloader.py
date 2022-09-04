0df6bebe6232af606271ecf4e206334fbdb0546e
techreative <nathanielejones@hotmail.com>
2022-09-04 19:08:18 -0400

    added ability to download only audio, only video, or both.

--- ytdownloader.py
+++ ytdownloader.py
@@ -3,13 +3,22 @@ from sys import argv
 
 link = input("What is the URL you want to download?")
 
-choice = input('Enter 1 to download audio only or 2 for video: ')
+choice = input(
+    'Enter \n 1 to download audio only \n 2 for video\n 3 for both\n: ')
 
 if choice == '1':
     yt = YouTube(link)
-    yd = yt.streams.get_audio_only()
+    yda = yt.streams.get_audio_only()
+    yda.download('/Users/folder/Documents/YT/audio')
+elif choice == '2':
+    yt = YouTube(link)
+    ydv = yt.streams.get_highest_resolution()
+    ydv.download('/Users/folder/Documents/YT/video')
 else:
     yt = YouTube(link)
-    yd = yt.streams.get_highest_resolution()
+    yda = yt.streams.get_audio_only()
+    ydv = yt.streams.get_highest_resolution()
+    yda.download('/Users/folder/Documents/YT/audio')
+    ydv.download('/Users/folder/Documents/YT/video')
 
-yd.download('target folder here')
+# yd.download('/Users/NEJ/Documents/YT')
